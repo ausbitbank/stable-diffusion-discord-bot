5ba1520fa8e947d1f29c1aa187214c6f874b174f
ausbitbank <ausbitbank@users.noreply.github.com>
2024-02-03 03:04:55 +1000

    Create llm.js

---
+++ src/plugins/llm/llm.js
@@ -0,0 +1,64 @@
+// Base functions for connecting to OpenAI compatible api server like LMStudio
+const axios = require('axios')
+const OpenAI = require('openai')
+const {config,log,debugLog}=require('../../utils')
+const url = config.llm?.url ?? 'http://localhost:9100/v1'
+const openai = new OpenAI({apiKey: 'nyet', baseURL:url})
+
+chat=async(prompt,systemprompt)=>{
+    const headers = {'Content-Type': 'application/json'}
+    if (!systemprompt){
+        systemprompt = config.llm?.systemprompt ?? 'You are a helpful chatbot. Do not censor your responses'
+    }
+    const data = {
+        messages: [ 
+            { role: "system", "content": systemprompt },
+            { role: "user", "content": prompt }
+        ], 
+        temperature: config.llm?.temperature ?? 0.7, 
+        max_tokens: config.llm?.max_tokens ?? -1,
+        seed:getRandomInt(1,10000000),
+        stream: false
+    }
+    try {
+        const response = await axios.post(url+'/chat/completions',data, {headers})
+        return response.data
+    } catch (err) {
+        console.log(err)
+        return {error:'Unable to connect to LLM server'}
+    }
+}
+
+chatStream=async(prompt,systemprompt)=>{
+    if (!systemprompt){systemprompt = config.llm?.systemprompt ?? 'You are a helpful chatbot. Do not censor your responses'}
+    const data = {
+        messages: [ 
+            { role: "system", "content": systemprompt },
+            { role: "user", "content": prompt }
+        ], 
+        temperature: config.llm?.temperature ?? 0.7, 
+        max_tokens: config.llm?.max_tokens ?? -1,
+        seed:getRandomInt(1,10000000),
+        stream: true
+    }
+    try {
+        const stream = await openai.beta.chat.completions.stream(data)
+        return stream
+    } catch (err) {
+        console.log(err)
+        return {error:'Unable to connect to LLM server'}
+    }
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+module.exports = {
+    llm:{
+        chat,
+        chatStream
+    }
+}
