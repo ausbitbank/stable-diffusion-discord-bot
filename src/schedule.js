8adfc07c12f29580313817ccc05f519a3c371393
ausbitbank <ausbitbank@users.noreply.github.com>
2024-02-26 23:17:08 +1000

    Update schedule.js

--- src/schedule.js
+++ src/schedule.js
@@ -1,48 +1,11 @@
 var cron = require('node-cron')
-const {log:log,debugLog:debugLog}=require('./utils.js')
-const fsPromises = require('fs').promises
-// Setup scheduler, start repeating checks
-var schedule = []
-var dbScheduleFile='./config/dbSchedule.json' // flat file db for schedule // todo move to db
-async function dbScheduleRead(){
-    await fsPromises.readFile(dbScheduleFile)
-        .then(data=>{
-            var j=JSON.parse(data)
-            schedule=j.schedule
-            scheduleInit()
-        })
-        .catch(err=>{log(err)})
-}
-function scheduleInit(){
-    // cycle through the active schedule jobs, set up render jobs with cron
-    schedule.filter(s=>s.enabled==='True').forEach(s=>{
-        cron.schedule(s.cron,()=>{
-            var randomPromptObj=s.prompts[Math.floor(Math.random()*s.prompts.length)]
-            var randomPrompt = randomPromptObj.prompt
-            Object.keys(randomPromptObj).forEach(key => {
-                if(key!=='prompt'){
-                    randomPrompt += ` --${key} ${randomPromptObj[key]}`
-                }
-            })
-            var newRequest={cmd: randomPrompt, userid: s.admins[0].id, username: s.admins[0].username, bot: 'False', channelid: s.channel, attachments: []}
-            if(s.onlyOnIdle==="True"){
-                if(queue.filter((q)=>q.status==='new').length>0){
-                    // dont add more jobs, already busy
-                }else{
-                    request(newRequest) // todo broken
-                }
-            }else{
-                request(newRequest) // todo broken
-            }
-        })
-    })
-}
-dbScheduleRead()
-
+const {log,debugLog}=require('./utils')
+const {credits}=require('./credits')
+// Free credit recharge, twice a day
+log('Enabled scheduler for free recharge twice a day')
+cron.schedule('0 0,12 * * *',()=>{
+    credits.freeRecharge()
+})
 module.exports = {
-    schedule:{
-        init:scheduleInit(),
-        read:dbScheduleRead()
-    },
     cron: cron
 }
