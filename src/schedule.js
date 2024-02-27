1459107750e5c0409b8e379d843dba30b57f3851
ausbitbank <ausbitbank@users.noreply.github.com>
2024-02-27 21:55:16 +1000

    Update schedule.js

--- src/schedule.js
+++ src/schedule.js
@@ -1,10 +1,15 @@
 var cron = require('node-cron')
 const {log,debugLog}=require('./utils')
 const {credits}=require('./credits')
-// Free credit recharge, twice a day
-log('Enabled scheduler for free recharge twice a day')
+const {invoke}=require('./invoke')
 cron.schedule('0 0,12 * * *',()=>{
+    // Free credit recharge, twice a day
     credits.freeRecharge()
+})
+cron.schedule('*/5 * * * *',()=>{
+    // Rescan all invoke hosts, models, status every 5 minutes
+    debugLog('Rescanning all invoke hosts every 5 minutes')
+    invoke.init()
 })
 module.exports = {
     cron: cron
