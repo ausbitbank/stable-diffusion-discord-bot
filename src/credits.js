9174bc02fb04070f90ac7908b00b768f67a81f29
ausbitbank <ausbitbank@users.noreply.github.com>
2024-03-04 12:29:16 +1000

    Update credits.js

--- src/credits.js
+++ src/credits.js
@@ -1,20 +1,26 @@
 // Handle all functions related to credit state management
 const {log,debugLog,config} = require('./utils.js')
 const {db,User,Op}=require('./db.js')
-const defaultCredits = 100.00
+const { isObject } = require('lodash')
+const defaultCredits = config.credits.default??100.00
 
 balance=async(user)=>{
     // Check if a user exists in database, create new user if it doesnt, return true/false if the user already existed
-    let [usr,created] = await User.findOrCreate({where:{discordID: user},defaults:{credits:defaultCredits}})
+    // first function trigger when interacting with new user
+    // accept both job.creator object and plain id
+    let userid,username
+    if(isObject(user)){userid=user.discordid;username=user.username}else{userid=user;username=null}
+    let [usr,created] = await User.findOrCreate({where:{discordID: userid},defaults:{credits:defaultCredits,username:username}})
+    if(created){debugLog('Created new account '+username+' '+userid)}
     return parseFloat((usr.credits).toFixed(2))
 }
 
 decrement=async(user,amount=1)=>{
-    let usr = await User.findOrCreate({where:{discordID:user},defaults:{credits:defaultCredits}})
+    let usr = await User.findOrCreate({where:{discordID:user.discordid},defaults:{credits:defaultCredits,username:user.username}})
     let newcredits = parseFloat((usr[0].credits - amount).toFixed(2))
     if(newcredits<0){newcredits=0}
-    await User.update({credits:newcredits},{where:{discordID:user}})
-    debugLog('Credit removed: -'+amount+' from '+user+' , Balance: '+newcredits)
+    await User.update({credits:newcredits},{where:{discordID:user.discordid}})
+    debugLog('Credit removed: -'+amount+' from '+user.discordid+' , Balance: '+newcredits)
     return newcredits
 }
 
@@ -27,24 +33,12 @@ increment=async(user,amount=1)=>{
 }
 
 transfer=async(from,to,amount=1)=>{
-    await increment(to,amount)
+    // from is a job.creator object, to is just id
+    let response = await increment(to,amount)
+    debugLog(response)
     let bal = await decrement(from,amount)
-    /*
-    let usrFrom = await User.findOrCreate({where:{discordID:from},defaults:{credits:defaultCredits}})
-    let usrTo = await User.findOrCreate({where:{discordID:to},defaults:{credits:defaultCredits}})
-    debugLog(usrFrom.credits)
-    debugLog(usrTo.credits)
-    let newcreditsTo = parseFloat((usrTo.credits + amount).toFixed(2))
-    let newcreditsFrom = parseFloat((usrFrom.credits - amount).toFixed(2))
-    debugLog(newcreditsFrom)
-    debugLog(newcreditsTo)
-    //await usrTo.update({credits:newcreditsTo})
-    await User.update({credits:newcreditsTo},{where:{discordID:to}})
-    await User.update({credits:newcreditsFrom},{where:{discordID:from}})
-    //await usrFrom.update({credits:newcreditsFrom})
-    debugLog('Credit added: +'+amount+' to '+to+' from '+from)
-    */
-    debugLog('Credit added: +'+amount+' to '+to+' from '+from)
+    debugLog(bal)
+    debugLog('Credit transferred: +'+amount+' to '+to+' from '+from)
     return bal
 }
 
@@ -56,6 +50,7 @@ freeRecharge=async()=>{
     let rechargeAmount = config.freeRecharge.amount??20
     let result = await User.update({credits:rechargeAmount},{where:{credits:{[Op.lt]:minimumBalance},banned:false}})
     debugLog('Free recharge completed')
+    debugLog(result)
     return result
 }
 
