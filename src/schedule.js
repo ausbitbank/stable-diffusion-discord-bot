b73308e17ab6b653538ab2e67ebb04a7c59ced17
ausbitbank <ausbitbank@users.noreply.github.com>
2024-03-17 22:44:49 +1000

    Update schedule.js

--- src/schedule.js
+++ src/schedule.js
@@ -3,12 +3,12 @@ const {log,debugLog}=require('./utils')
 const {credits}=require('./credits')
 const {invoke}=require('./invoke')
 cron.schedule('0 0,12 * * *',()=>{
-    // Free credit recharge, twice a day
+    // credit recharge, twice a day
     credits.freeRecharge()
+    credits.memberRecharge()
 })
 cron.schedule('*/5 * * * *',()=>{
     // Rescan all invoke hosts, models, status every 5 minutes
-    debugLog('Rescanning all invoke hosts every 5 minutes')
     invoke.init()
 })
 module.exports = {
